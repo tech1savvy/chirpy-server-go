d47fbc214c5a5df24aae13e7975ea42daffdd231
tech1savvy <amankumar010604@gmail.com>
2026-04-09 06:52:33 +0530

    feat: update metrics endpoints to /admin routes

--- main.go
+++ main.go
@@ -23,8 +23,9 @@ func main() {
 	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerGetMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerResetMetrics)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerGetMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetMetrics)
 
 	wrappedMux := middlewareLog(mux)
 
